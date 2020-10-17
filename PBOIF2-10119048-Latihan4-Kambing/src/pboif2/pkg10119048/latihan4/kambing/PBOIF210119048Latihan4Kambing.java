ac48fb4de80a69ef62f506dcd7e2ef55dac2a6ec
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 23:21:57 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan4-Kambing/src/pboif2/pkg10119048/latihan4/kambing/PBOIF210119048Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Menampilkan Jumlah Kambing
+ */
+public class PBOIF210119048Latihan4Kambing {
+public void tambahKambing(){
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOIF210119048Latihan4Kambing kambingJantan = new PBOIF210119048Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
